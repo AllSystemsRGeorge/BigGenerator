159f253804af9a907ea6de511eb8fa8257a6e64a
AllSystemsRGeorge <96609501+AllSystemsRGeorge@users.noreply.github.com>
2022-07-11 22:11:05 -0700

    properly linked CSS and JS

--- assets/JS/generator.js
+++ assets/JS/generator.js
@@ -1,24 +1,18 @@
-var button = document.getElementById('myBtn')
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-button.addEventListener('click', generatePassword)
-
-var result = document.getElementById('passwordContainer')
 function generatePassword() {
-   var smallLetter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-   var capitalLetter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-   var symbols = ['!','@','#','$','%','&','*','+','?']
-   var numbers = ['0','1','2','3','4','5','6','7','8','9']
-   var array = []
+   
+}
 
-   var allDigits = [...smallLetter, ...capitalLetter, ...symbols, ...numbers]
-   console.log(allDigits.length);
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-   for (let i = 0; i < allDigits.length; ++i) {
-
-      var pick = Math.floor(Math.random() * allDigits.length)
-      array.push(allDigits[pick])  
-   }
-   array.length = 8
-   result.innerText = array.join('')
+  passwordText.value = password;
 
 }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
